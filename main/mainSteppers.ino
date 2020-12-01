666874caef95dcb8b697da296d3cb9dd07808653
Pablo C. Ruíz <pcruiher089@gmail.com>
2020-11-30 20:58:06 -0600

    fire

---
+++ main/mainSteppers.ino
@@ -0,0 +1,16 @@
+#include <Stepper.h>
+
+#define PASOS 100
+Stepper stepper(PASOS, 8, 9, 10, 11);
+
+int previous = 0;
+
+void setup(){
+  stepper.setSpeed(30);
+}
+
+void loop() {
+  int val = 10;
+  stepper.step(val - previous);
+  previous = val;
+}
